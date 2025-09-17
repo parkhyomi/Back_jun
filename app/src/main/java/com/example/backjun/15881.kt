fef7d3f7d57538327be23acc6d8d03d3157e2701
parkhyomi <phyomi3@naver.com>
2025-09-17 18:06:04 +0900

    15881 추가 제안 작상

--- app/src/main/java/com/example/backjun/15881.kt
+++ app/src/main/java/com/example/backjun/15881.kt
@@ -22,6 +22,7 @@ fun main(){
 
     while ( index < list.size-1 ) {
         if (list[index]==answarelist[0] && list[index+1]==answarelist[1]&& list[index+2]==answarelist[2]&& list[index+3]==answarelist[3]) {
+            // if (list.subList(i, i+4) == answarelist) 이런식으로 작성하면 더 효율적으로 작성할 수 있음
             index += 4
             count += 1
         }
@@ -31,3 +32,13 @@ fun main(){
     }
     print(count)
 }
+
+/**
+ * 내장 함수를 사용한다면 이런식으로도 가능 함
+ *     val N = readln().toInt()
+ *     val list = readln().toList().take(N)
+ *     val answarelist = listOf('p','P','A','p')
+ *
+ *     val count = list.windowed(4, 1).count { it == answarelist }
+ *     print(count)
+ */

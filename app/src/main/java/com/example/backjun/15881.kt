a679742bd9d5dfbc7c58bbecc4ab713644ff81ee
parkhyomi <phyomi3@naver.com>
2025-09-17 18:03:58 +0900

    15881  해결

---
+++ app/src/main/java/com/example/backjun/15881.kt
@@ -0,0 +1,33 @@
+package com.example.backjun
+
+/**
+ * 물건의 총 개수 n
+ * 사과는 A로, 파인애플은 P로, 펜은 p로 대소문자를 구분
+ *
+ * pPAp
+ */
+
+/**
+ * pPAp가 나온다면 i+4 부터 시작 / 없다면 i+1
+ * 1초 안에 내장 함수 되도록이면 안쓰게
+ * 배열에 pPAp 저장
+ * while index != realindex -> if(list[i..i+3] == array[i..i+3] ) index+3  count+= 1 else index=1 count+=1
+ */
+fun main(){
+    val N = readln().toInt()
+    val list = readln().toList().take(N)
+    val answarelist = listOf('p','P','A','p')
+    var index = 0
+    var count = 0
+
+    while ( index < list.size-1 ) {
+        if (list[index]==answarelist[0] && list[index+1]==answarelist[1]&& list[index+2]==answarelist[2]&& list[index+3]==answarelist[3]) {
+            index += 4
+            count += 1
+        }
+        else {
+            index += 1
+        }
+    }
+    print(count)
+}
